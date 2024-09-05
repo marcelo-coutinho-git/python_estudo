56ea79e330eae66839080fdc165e994697ceacf5
Marcelo Coutinho <marcelocoutinho@MacBook-Air-de-Marcelo.local>
2024-09-05 18:58:51 -0300

    Curso Python

---
+++ Curso_Python/aula27.py
@@ -0,0 +1,14 @@
+#Listas
+    # Armazenar mais de uma informação em variavel
+    # Manter a sequencia ddos dados em uma variavel
+    
+cidade1 = 'RJ'
+cidade2 = 'SP'
+cidade3 = 'Salvador'
+
+cidades = ['RJ', 'SP', 'Salvador', 'BH']
+#            0     1        2        3
+
+cidades.append ('Brasilia')
+
+print(cidades[4])
